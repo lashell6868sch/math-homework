3e0b8c72a55ac8949a099543a67a26b06bda7371
lashell6868sch <lashell6868sch@gmx.com>
2025-03-08 21:01:01 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int num1 = 2;
+    int num2 = 3;
+    float result = (float)num1 / num2;
+    printf("The result is: %f\n", result);
+    return 0;
+}
